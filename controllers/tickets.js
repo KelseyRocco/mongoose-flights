14b83463ffc276e9aea500f4e5328dcb41a232d3
KelseyRocco <kelseyroccoh@gmail.com>
2021-05-06 20:10:52 -0400

    "missing closing form"

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,19 +1,28 @@
-// const { SchemaType } = require('mongoose');
-// const Flight = require('../models/flight');
-// const Ticket = require('../models/ticket');
+const { SchemaType } = require('mongoose');
+const Flight = require('../models/flight');
+const Ticket = require('../models/ticket');
 
 
-// module.exports = {
-//     create,    
-//     };
+module.exports = {
+    new: newTicket,
+    create
+    };
+
+    function newTicket(req, res){
+        
+    }
 
 
-//     function create(req, res) {
-//     Flight.findById(req.params.id, function(err, flight) {
-//         flight.tickets.push(req.body);
-//         flight.save(function(err) {
-//         res.redirect(`/flights/${flight._id}`);
-//         });
-//     });
-// }
+    function create(req, res) {
+    Flight.findById(req.params.id, function(err, flight) {
+        req.body.flight = flight._id;
+        Ticket.create(req.body, function(err, ticket) {
+            Ticket.find({ flight: flight._id}, function (err, tickets) {
+                flight.save(function(err){
+                    res.redirect(`/flights/${flight._id}`)
+                });
+            });
+        })
+    });
+}
 
